85c39d8a4b964947b33e44b37a4eb79e3edbfd5d
rmpaschal <85592031+rmpaschal@users.noreply.github.com>
2022-04-20 16:32:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $.getJSON("./data.json", function(spotsData){
+    for(let i = 0; i < spotsData.length; i++){
+        $("#table-body").before(`<tr><td>${spotsData[i].name}</td><td>${spotsData[i].description}</td><td ><a href="https://www.google.com/maps?q=${spotsData[i].location[0]},${spotsData[i].location[1]}">Link</a></td></tr>`)
+}
+          
+})})
